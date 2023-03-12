caa0ef69c217c67be80579d79e5f128b3c059654
Igor-CA <122459409+Igor-CA@users.noreply.github.com>
2023-03-12 20:22:41 -0300

    Calculator

--- The Odin Project/2 - Calculator/scrips.js
+++ The Odin Project/2 - Calculator/scrips.js
@@ -2,12 +2,18 @@ let dotIn = false
 let numberOne = null
 let numberTwo = null
 let operator = null
+let isResetable = true
 
 let numbersInput = document.querySelectorAll(".number")
 
 numbersInput.forEach((number)=>{
     number.addEventListener('click', ()=>{
         let display = document.querySelector('#display')
+        if(isResetable){
+            isResetable = false
+            display.innerText = null
+            dotIn = false
+        }
         if(display.innerText.length < 9){ 
             if(number.value === '.' && !dotIn){
                 dotIn = true
@@ -23,20 +29,49 @@ let operationsInput = document.querySelectorAll(".operators")
 operationsInput.forEach((operation) => {
     operation.addEventListener('click', () => {
         let display = document.querySelector('#display')
-        if(numberOne === null){ numberOne = +display.innerText }
-        else{ numberTwo = +display.innerText }
+        isResetable = true
+        if(numberOne === null){
+            numberOne = (+display.innerText)
+        }else{
+            doCalculations()
+        }
         operator = operation.value
-        result = realizeOperation(numberOne, numberTwo, operator)
-        if(numberTwo === null){
-            numberOne = result
-            numberTwo = null
-        } 
-        display.innerText = result
     })
 })
 
+function doCalculations(){
+    let display = document.querySelector('#display')
+    numberTwo = (+display.innerText)
+    let value = realizeOperation(numberOne, numberTwo, operator)
+    numberOne = value
+    display.innerText = value
+}
+
+let equalsBtn = document.querySelector('#equal')
+
+equalsBtn.addEventListener('click', doCalculations)
+
+let reset = document.querySelector('#ac')
+
+reset.addEventListener('click', () => {
+    let display = document.querySelector('#display')
+    display.innerText = null
+    numberOne = null
+    numberTwo = null
+    operator = null
+})
+
+let revert = document.querySelector('#invert')
+
+revert.addEventListener('click', ()=>{
+    let display = document.querySelector('#display')
+    let value = numberTwo = (+display.innerText)
+    value *= (-1)
+    display.innerHTML = value
+})
+
 function add(number1, number2){ return number1+number2 }
-function subtract(number1, number2){ return number2-number1 }
+function subtract(number1, number2){ return number1-number2 }
 function multiply(number1, number2){ return number1*number2 }
 function divide(number1, number2){ 
     if(number2 === 0) return "can't divide by 0"
@@ -47,5 +82,5 @@ function realizeOperation(number1, number2, operator){
     if(operator === '+'){ return add(number1, number2)}
     else if(operator === '-'){ return subtract(number1, number2)}
     else if(operator === '*'){ return multiply(number1, number2)}
-    else { return divide(number1, number2) }
+    else if(operator === '/'){ return divide(number1, number2) }
 }
