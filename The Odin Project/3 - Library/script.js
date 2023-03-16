ff3fe50c1a45405c8229bd2b778f3025852edb7d
Igor-CA <122459409+Igor-CA@users.noreply.github.com>
2023-03-15 21:30:45 -0300

    Finish library project

--- The Odin Project/3 - Library/script.js
+++ The Odin Project/3 - Library/script.js
@@ -15,27 +15,79 @@ Book.prototype.printInfo = function(){
 
 Book.prototype.createHtml = function(){
     let library = document.querySelector('#library')
-    let bookDiv = document.createElement('div')
-    bookDiv.classList += 'book'
-    bookDiv.innerHTML = 
+    this.bookDiv = document.createElement('div')
+    this.bookDiv.classList += 'book'
+    this.bookDiv.innerHTML = 
     `
-        <p>${this.title}</p>
+        <h2>${this.title}</h2>
         <p>${this.author}</p>
-        <p>${this.pages}</p>
-        <p>${this.read}</p>
+        <p>${this.numberPages}</p>
         <input type="button" value="${this.read}" class="read">
         <input type="button" value="remove" class="remove">
     `
-    library.appendChild(bookDiv)
+    library.appendChild(this.bookDiv)
+}
+Book.prototype.addListeners = function(){
+    
+    let readButtom = this.bookDiv.querySelector(".read")
+    readButtom.addEventListener('click',()=>{
+        (readButtom.value === 'Read')? readButtom.value = 'Not read' 
+                                     : readButtom.value = 'Read'
+    })
+    
+    let removeBtn = this.bookDiv.querySelector(".remove")
+    removeBtn.addEventListener('click', ()=>{
+        this.bookDiv.style.display = 'none'
+        let index = myLibrary.indexOf(this)
+        myLibrary.splice(index, 1)
+    })
+}
+
+
+
+
+let newBookBtn = document.querySelector('#add');
+let closeFormBtn = document.querySelector('#closeForm');
+let addBookBtn = document.querySelector('#confirmAdd');
+
+function displayForm(){
+    let form = document.querySelector('#form')
+    form.classList.toggle('invisible')
+}
+
+function closeForm(){
+    let form = document.querySelector('#form')
+    form.classList.toggle('invisible')
 }
 
 function addBookToLibrary(){
-    let name = prompt()
-    let author = prompt()
-    let pages = prompt()
-    let read = confirm()
+    let nameInput = document.querySelector('#name')
+    let authorInput = document.querySelector('#author')
+    let pagesInput = document.querySelector('#pages')
+    let readInput = document.querySelector('#status')
+
+    let name = nameInput.value
+    let author = authorInput.value
+    let pages = pagesInput.value
+    let read = readInput.checked
+
+    nameInput.value = null
+    authorInput.value = null
+    pagesInput.value = null
+    readInput.checked = null
+
     let book = new Book(name, author, pages, read)
     myLibrary.push(book)
+    book.createHtml()
+    book.addListeners()
+    closeForm()
 }
+
+newBookBtn.addEventListener('click',displayForm);
+closeFormBtn.addEventListener('click', closeForm);
+addBookBtn.addEventListener('click', addBookToLibrary);
+
+
+
 
 let kaguya = new Book('kaguya', 'Aka Akasaka', 225, true)
